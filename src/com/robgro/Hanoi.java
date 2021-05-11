0c0210b1262a6fe29f1b4df6f702dd40127e9e23
RobGro <r.grodzki74@gmail.com>
2021-05-11 12:38:59 +0100

    Hanoi's Tower movement solution with recurrence method. It shows all moves needed to find out the solution, and how long does it take for your CPU

---
+++ src/com/robgro/Hanoi.java
@@ -0,0 +1,33 @@
+package com.robgro;
+
+public class Hanoi {
+
+    public static int hanoi(int size, int initialPlace, int finalPlace) {
+        if (size == 1) {
+            System.out.println("[element 1] from " + initialPlace + " move to " + finalPlace);
+            return 1;
+        }
+        int totalMoves = 0;
+        // if initialPlace = 1, finalPlace = 2 ==> temporaryPlace = 3, all together = 6
+        int temporaryPlace = 6 - finalPlace - initialPlace;
+        totalMoves += hanoi(size - 1, initialPlace, temporaryPlace);
+        System.out.println("[element " + size + "] from " + initialPlace + " move to " + finalPlace);
+        totalMoves++;
+        totalMoves += hanoi(size - 1, temporaryPlace, finalPlace);
+        return totalMoves;
+    }
+
+    public static void main(String[] args) {
+
+        // how long does it take ?
+        long start = System.nanoTime();
+
+        // main method
+        int totalMoves = hanoi(5,1,2);
+        System.out.println("total moves = " + totalMoves);
+
+        long elapsed = System.nanoTime() - start;
+        System.out.println("Hanoi's Tower estimates solution took " + elapsed + " CPU nano ticks");
+
+    }
+}
